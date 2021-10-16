0f6a150eb81911887f015e7c5909786c8c27d12d
JamesYew26 <junchee0206@gmail.com>
2021-10-16 15:47:06 +0800

    Mobile App Dev without map

--- app/src/main/java/com/example/mobileappdevassignment/MainActivity.java
+++ app/src/main/java/com/example/mobileappdevassignment/MainActivity.java
@@ -2,13 +2,84 @@ package com.example.mobileappdevassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mLocation1;
+    private TextView mLocation2;
+    private TextView mLocation3;
+    private TextView mLocation4;
+    private TextView mLocation5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openLocation1(View view) {
+        mLocation1 = findViewById(R.id.location1);
+        String loc = mLocation1.getText().toString();
+        Uri addressUri=Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW,addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void openLocation2(View view) {
+        mLocation2 = findViewById(R.id.location2);
+        String loc = mLocation2.getText().toString();
+        Uri addressUri=Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW,addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void openLocation3(View view) {
+        mLocation3 = findViewById(R.id.location3);
+        String loc = mLocation3.getText().toString();
+        Uri addressUri=Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW,addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void openLocation4(View view) {
+        mLocation4 = findViewById(R.id.location4);
+        String loc = mLocation4.getText().toString();
+        Uri addressUri=Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW,addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void openLocation5(View view) {
+        mLocation5 = findViewById(R.id.location5);
+        String loc = mLocation5.getText().toString();
+        Uri addressUri=Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW,addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
     }
 }
